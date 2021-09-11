35425f4520a6ef73ce2496f88c556f1e0c6e735b
Sam Schirmer <sschirmer@hatchbuck.com>
2021-09-11 10:11:55 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function(){
+    sayHello();
+ });
+
+ async function sayHello() {
+    const l = ['H','e','l','l','o','\u00A0','W','o','r','l','d','.'];
+    s = document.getElementById('hello');
+    for (let i=0; i<l.length; i++) {
+        s.innerText = `${s.innerText}${l[i]}`;
+        await new Promise(r => setTimeout(r, 200));
+    }
+ }
