bb1bf90dc011c587cb4bc844c255a9b026f454b0
Sam Schirmer <sschirmer@hatchbuck.com>
2021-09-15 10:50:53 -0500

    cleanup/tweaks

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded',function(){
 
  async function sayHello() {
     const l = ['H','e','l','l','o','\u00A0','W','o','r','l','d','.'];
-    s = document.getElementById('hello');
+    let s = document.getElementById('hello');
     for (let i=0; i<l.length; i++) {
         s.innerText = `${s.innerText}${l[i]}`;
         await new Promise(r => setTimeout(r, 200));
